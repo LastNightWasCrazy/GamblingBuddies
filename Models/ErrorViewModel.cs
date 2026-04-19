de12b80dfafe225eb2941831c47e5e3e7dfa69e5
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2026-04-20 01:42:00 +0200

    pierwszy commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamblingBuddies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
