c7a839fb0192dc9272d066651eba3346ab6d19f1
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2026-04-29 00:27:45 +0200

    autoryzacja

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,10 +1,67 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace GamblingBuddies.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string login, string password)
+        {
+            var user = _context.SystemUsers
+                .FirstOrDefault(u => u.Login == login && u.IsActive);
+
+            if (user == null)
+            {
+                ViewBag.Error = "Nieprawidłowy login lub hasło";
+                return View();
+            }
+
+            if (user.PasswordHash != password)
+            {
+                ViewBag.Error = "Nieprawidłowy login lub hasło";
+                return View();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.SystemUserId.ToString()),
+                new Claim(ClaimTypes.Name, user.Login)
+            };
+
+            var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync("MyCookieAuth", principal);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("MyCookieAuth");
+            return RedirectToAction("Login", "Account");
+        }
+
+        public IActionResult AccessDenied()
         {
             return View();
         }
