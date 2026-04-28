e28a147fea38b47dd2e0a33314a81cf6a2447d31
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2026-04-28 17:01:56 +0200

    GB

--- obj/Debug/net9.0/GamblingBuddies.AssemblyInfo.cs
+++ obj/Debug/net9.0/GamblingBuddies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GamblingBuddies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd89d253fef654908b27dc525109d2ad139bf0da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c1fa6482843320a9a4c6a49269930113e20503")]
 [assembly: System.Reflection.AssemblyProductAttribute("GamblingBuddies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GamblingBuddies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
