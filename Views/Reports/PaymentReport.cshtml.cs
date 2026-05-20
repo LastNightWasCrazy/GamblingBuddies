61e11b5fb07f775bd20e600dd0c88d9cf891f3af
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2026-05-20 13:05:27 +0200

    lol

---
+++ Views/Reports/PaymentReport.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GamblingBuddies.Views.Reports
+{
+    public class PaymentReportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
