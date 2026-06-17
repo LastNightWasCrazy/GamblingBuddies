1656c5a899e6817f05731b2ac04664fe6a26321d
XXXX <korenkiewicz.wojciech@o2.pl>
2026-06-17 22:22:48 +0200

    dodanie indeksu

---
+++ Migrations/20260617201526_AddPaymentsReportIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GamblingBuddies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentsReportIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+CREATE NONCLUSTERED INDEX IX_Payments_Status_CreatedAt_IncludeAmount
+ON dbo.Payments (PaymentStatusId, CreatedAt)
+INCLUDE (Amount);
+");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+DROP INDEX IF EXISTS IX_Payments_Status_CreatedAt_IncludeAmount
+ON dbo.Payments;
+");
+        }
+    }
+}
