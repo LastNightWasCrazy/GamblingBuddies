797fb5408805f0e73853f44a376b6053f64f6b71
Bartosz Śnaidkowski <bsniadkowski2004@gmail.com>
2026-06-18 00:46:49 +0200

    baza index

--- DbScripts/SBD.sql
+++ DbScripts/SBD.sql
@@ -51,3 +51,57 @@ JOIN sys.tables t
 WHERE t.name = 'SystemUsers'
   AND i.name IS NOT NULL;
 GO
+
+-- Indeksy dla Payments
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.indexes
+    WHERE name = 'IX_Payments_CreatedAt_Status'
+      AND object_id = OBJECT_ID('dbo.Payments')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX IX_Payments_CreatedAt_Status
+    ON dbo.Payments (CreatedAt, PaymentStatusId)
+    INCLUDE (Amount, PaidAt, ReservationId, PaymentMethodId);
+END;
+GO
+
+-- Sprawdzenie indeksów dla Payments
+SELECT
+    t.name AS TableName,
+    i.name AS IndexName,
+    i.type_desc AS IndexType,
+    i.is_unique AS IsUnique
+FROM sys.indexes i
+JOIN sys.tables t
+    ON i.object_id = t.object_id
+WHERE t.name = 'Payments'
+  AND i.name IS NOT NULL;
+GO
+
+-- Indeks optymalizujący podgląd i filtrowanie rezerwacji
+IF NOT EXISTS (
+    SELECT 1
+    FROM sys.indexes
+    WHERE name = 'IX_Reservations_Status_ReservedAt'
+      AND object_id = OBJECT_ID('dbo.Reservations')
+)
+BEGIN
+    CREATE NONCLUSTERED INDEX IX_Reservations_Status_ReservedAt
+    ON dbo.Reservations (ReservationStatusId, ReservedAt DESC)
+    INCLUDE (PlayerId, GameSessionId);
+END;
+GO
+
+-- Sprawdzenie indeksów dla Reservations
+SELECT
+    t.name AS TableName,
+    i.name AS IndexName,
+    i.type_desc AS IndexType,
+    i.is_unique AS IsUnique
+FROM sys.indexes i
+JOIN sys.tables t
+    ON i.object_id = t.object_id
+WHERE t.name = 'Reservations'
+  AND i.name IS NOT NULL;
+GO
